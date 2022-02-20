e5005c07802d7c4e87c8d994fa7880e750d0276c
Atharv Parmar <atharvp45@gmail.com>
2022-02-20 13:24:09 +0530

    Modified searcher file to search with exact name

--- Assignment_3/Searcher.java
+++ Assignment_3/Searcher.java
@@ -1,8 +1,21 @@
 package Assignment_3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Searcher {
     public static int search(String s,int cind){
-        int index = 0;
+        int index = -1;
+        ArrayList<String> inf = new ArrayList<String>();
+        List<ArrayList<String>> filelist = new ArrayList<>();
+        filelist = FileListCreator.lister();
+        int count = GiveCount.counter();
+        for(int i=0;i<count;i++){
+            inf = filelist.get(i);
+            if(inf.contains(s)){
+                index = i;
+            }
+        }
 
         return index;
     }
